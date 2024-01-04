d80a4df877e26d0dd7df14e49f818b1f958cdbd0
Chenna.Sreenu <csrinu236@gmail.com>
2024-01-04 11:43:29 +0530

    zsdasd

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import Home from "./Home";
 import { FocusContext, useFocusable } from "@noriginmedia/norigin-spatial-navigation";
 import { publish } from "./utils/event";
 
 function App() {
+  const getFPS = () => new Promise((resolve) => requestAnimationFrame((t1) => requestAnimationFrame((t2) => resolve(1000 / (t2 - t1)))));
+
+  // Calling the function to get the FPS
+
+  const [frameRate, setframeRate] = useState(0);
+
+  useEffect(() => {
+    getFPS().then((fps) => setframeRate(fps));
+  }, []);
+
   const { ref, setFocus, focusKey, getCurrentFocusKey } = useFocusable({});
 
   useEffect(() => {
@@ -53,6 +63,7 @@ function App() {
     <FocusContext.Provider value={focusKey}>
       <section ref={ref} className="section">
         <div className="section-center">
+          <h1>{frameRate}</h1>
           <Home></Home>
         </div>
       </section>
