a0f8cdcb97f2cfb6f7ed0ff47e4b596abd6b77d6
Chenna.Sreenu <csrinu236@gmail.com>
2024-01-05 11:53:32 +0530

    hjjhb

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { init } from "@noriginmedia/norigin-spatial-navigation";
 
 init({
   // debug: true
-  throttle: 125,
+  throttle: 175,
 });
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
